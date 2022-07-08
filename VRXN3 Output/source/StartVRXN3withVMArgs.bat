8e72085e2140c37c1cb66283451c2d6384d3461c
The Shadow Coder <38187218+BreathOfLife@users.noreply.github.com>
2022-07-08 11:20:01 -0500

    Update StartVRXN3withVMArgs.bat

--- VRXN3 Output/source/StartVRXN3withVMArgs.bat
+++ VRXN3 Output/source/StartVRXN3withVMArgs.bat
@@ -1,2 +1,2 @@
-java --add-opens java.desktop/sun.awt=ALL-UNNAMED -jar Virtual-RXN-3.jar
+java --add-opens java.desktop/sun.awt=ALL-UNNAMED -cp Virtual-RXN-3.jar;flatlaf-2.3.jar;j3dcore.jar;j3dutils.jar;jogamp-fat.jar;vecmath.jar Control.Engine
 exit
